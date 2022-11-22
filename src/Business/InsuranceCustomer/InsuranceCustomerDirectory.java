a0a04e851c92bdc0b0b5efbd9c5111c6d19c23f2
bhavana0414 <lingareddy.c@northeastern.edu>
2022-11-22 15:21:55 -0500

    Insurance directory

---
+++ src/Business/InsuranceCustomer/InsuranceCustomerDirectory.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.InsuranceCustomer;
+
+/**
+ *
+ * @author bhavana0414
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class InsuranceCustomerDirectory {
+
+    private List<InsuranceCustomer> insCust = new ArrayList<>();
+
+    public List<InsuranceCustomer> getInsCust() {
+        return insCust;
+    }
+
+    public void setInsCust(List<InsuranceCustomer> insCust) {
+        this.insCust = insCust;
+    }
+
+    @Override
+    public String toString() {
+        return "InsuranceCustomerDirectory{" + "insuranceCustomers=" + insCust + '}';
+    }
+
+}
