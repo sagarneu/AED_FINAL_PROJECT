f0b0cebc4cb5c26ca904dd02da745ad55de1cf88
sagred <sagred17@gmail.com>
2022-11-24 10:42:56 -0500

    NGO Admin

---
+++ src/Business/Organization/NGOAdminOrganization.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Organization;
+
+import Business.Role.NGOAdminRole;
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sagred
+ */
+public class NGOAdminOrganization extends Organization{
+
+    public NGOAdminOrganization() {
+        super(Organization.Type.NGOAdministrator.getValue());
+    }
+    
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new NGOAdminRole());
+        return roles;
+    }
+     
+}
