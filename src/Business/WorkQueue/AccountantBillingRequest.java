c56f088721ba8b5361749ded5d9571228f149089
sagred <sagred17@gmail.com>
2022-12-04 10:07:33 -0500

    Account billing Info

---
+++ src/Business/WorkQueue/AccountantBillingRequest.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.WorkQueue;
+
+
+import Business.Patient.Patient;
+
+/**
+ *
+ * @author sagred
+ */
+public class AccountantBillingRequest extends WorkRequest {
+
+    private double billingAmt;
+
+    private boolean hasIns;
+    private boolean isInsClmSettled;
+
+    Patient patient;
+
+    public double getBillingAmt() {
+        return billingAmt;
+    }
+
+    public void setBillingAmt(double billingAmt) {
+        this.billingAmt = billingAmt;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public boolean isHasIns() {
+        return hasIns;
+    }
+
+    public void setHasIns(boolean hasIns) {
+        this.hasIns = hasIns;
+    }
+
+    public boolean isIsInsClmSettled() {
+        return isInsClmSettled;
+    }
+
+    public void setIsInsClmSettled(boolean isInsClmSettled) {
+        this.isInsClmSettled = isInsClmSettled;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(billingAmt);
+    }
+
+}
