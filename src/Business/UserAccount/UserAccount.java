2760c6aa4ca92ff0131752f90c06b2d2f5c2b80b
sagred <sagred17@gmail.com>
2022-12-03 22:52:53 -0500

    User Account Role

---
+++ src/Business/UserAccount/UserAccount.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.UserAccount;
+
+import Business.Employee.Employee;
+import Business.Role.Role;
+import Business.WorkQueue.WorkQueue;
+
+/**
+ *
+ * @author sagred
+ */
+public class UserAccount {
+
+    private String uname;
+    private String pwd;
+    private Employee emp;
+    private Role role;
+    private WorkQueue wrkQ;
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public UserAccount() {
+        wrkQ = new WorkQueue();
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setEmp(Employee emp) {
+        this.emp = emp;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Employee getEmp() {
+        return emp;
+    }
+
+    public WorkQueue getWrkQ() {
+        return wrkQ;
+    }
+
+    @Override
+    public String toString() {
+        return uname;
+    }
+
+}
